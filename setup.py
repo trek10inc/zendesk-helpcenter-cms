ae8ffc81cc9cf5ec807e014f6f72990e9c9440a8
Jared Short <jaredlshort@gmail.com>
2018-04-24 11:43:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 import os
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
-from pip.download import PipSession
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+try: # for pip >= 10
+    from pip._internal.download import PipSession
+except ImportError: # for pip <= 9.0.3
+    from pip.download import PipSession
 
 version = '0.1.2'
 
